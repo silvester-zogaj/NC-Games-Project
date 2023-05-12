d98dd0bb001210e27275f4db8f3fd9af5201acfe
silvester-zogaj <silvester_9x@hotmail.co.uk>
2023-05-12 11:08:14 +0100

    setting up production

--- api/listen.js
+++ api/listen.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
 
-app.listen(9090, () => {
-  console.log('Server is listening on port 9090');
-});
+const {PORT = 9090} = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
